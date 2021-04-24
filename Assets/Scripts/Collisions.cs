5791928fc7824d4e8aea5f030e389c6f54bca2e6
Elias <eliasbrucart@hardgames.com.ar>
2021-04-24 18:11:31 -0300

    made the collision with the grave

---
+++ Assets/Scripts/Collisions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("El player colisiono con: " + collision.gameObject.tag);
+    }
+}
