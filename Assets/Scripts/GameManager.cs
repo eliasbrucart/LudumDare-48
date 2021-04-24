96e7b49ea2f35b4a1d9672202253dcabe36707b6
Elias <eliasbrucart@hardgames.com.ar>
2021-04-24 12:56:54 -0300

    The game manager is now a singelton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instanceGameManager;
+
+    public static GameManager Instance { get { return instanceGameManager; } }
+
+    void Awake()
+    {
+        if(instanceGameManager != null && instanceGameManager != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instanceGameManager = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
