354f77380d059920870782951524df7498756280
Julian Bega <julian.begamilrad@gmail.com>
2021-04-24 22:27:51 -0300

    Recharge magic

---
+++ Assets/Scripts/RechargeMagic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RechargeMagic : MonoBehaviour
+{
+    PlayerManager manager;
+
+    public int RechargeValue;
+    void Start()
+    {
+        manager = this.GetComponent<PlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (manager.rechargeMagic >= 1)
+            {
+                manager.rechargeMagic -= 1;
+                manager.liberateTrap += RechargeValue;
+                manager.smallTorch += RechargeValue;
+                manager.bigTorch += RechargeValue;
+                manager.digGrave += RechargeValue;
+                manager.freezeGhost += RechargeValue;
+            }
+        }
+    }
+}
