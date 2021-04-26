a3f09e67c4f0e2f9fabc25159942aa7b65ebb16c
Julian Bega <julian.begamilrad@gmail.com>
2021-04-26 11:21:59 -0300

    RandomGhostDeleteThemselves

---
+++ Assets/Scripts/TimeToDestroyThis.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToDestroyThis : MonoBehaviour
+{
+    public float timeToDeleteItself;
+
+    void Update()
+    {
+        timeToDeleteItself -= Time.deltaTime;
+        if (timeToDeleteItself <= 0) Destroy(this.gameObject);
+
+    }
+
+}
+
+
